65c5d5fafd48d8de44cf192fc740cf1e2a15d5d4
leesky <leehaneul0623@gmail.com>
2023-03-14 23:55:21 +0900

    feat : 웹 어댑터 구현

---
+++ src/main/java/cleanarchitecture/buckpal/adapter/in/web/SendMoneyController.java
@@ -0,0 +1,32 @@
+package cleanarchitecture.buckpal.adapter.in.web;
+
+import cleanarchitecture.buckpal.account.application.port.in.SendMoneyCommand;
+import cleanarchitecture.buckpal.account.application.port.in.SendMoneyUseCase;
+import cleanarchitecture.buckpal.account.domain.Account.AccountId;
+import cleanarchitecture.buckpal.account.domain.Money;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class SendMoneyController {
+
+  private final SendMoneyUseCase sendMoneyUseCase;
+
+  @PostMapping(path = "/accounts/send/{sourceAccountId}/{targetAccountId}/{amount}")
+  void sendMoney(
+      @PathVariable("sourceAccountId") Long sourceAccountId,
+      @PathVariable("targetAccountId") Long targetAccountId,
+      @PathVariable("amount") Long amount) {
+
+    SendMoneyCommand command = new SendMoneyCommand(
+        new AccountId(sourceAccountId),
+        new AccountId(targetAccountId),
+        Money.of(amount)
+    );
+
+    sendMoneyUseCase.sendMoney(command);
+  }
+}
