e00d892ff3985416a54981e6ac169339d759ac96
leesky <leehaneul0623@gmail.com>
2023-03-26 23:40:08 +0900

    fix : 웹 어댑터 어노테이션 추가

--- src/main/java/cleanarchitecture/buckpal/account/adapter/in/web/SendMoneyController.java
+++ src/main/java/cleanarchitecture/buckpal/account/adapter/in/web/SendMoneyController.java
@@ -4,11 +4,13 @@ import cleanarchitecture.buckpal.account.application.port.in.SendMoneyCommand;
 import cleanarchitecture.buckpal.account.application.port.in.SendMoneyUseCase;
 import cleanarchitecture.buckpal.account.domain.Account.AccountId;
 import cleanarchitecture.buckpal.account.domain.Money;
+import cleanarchitecture.buckpal.common.WebAdapter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@WebAdapter
 @RestController
 @RequiredArgsConstructor
 public class SendMoneyController {
